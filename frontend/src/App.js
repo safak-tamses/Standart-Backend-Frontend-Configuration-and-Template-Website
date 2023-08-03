0077aabcc640c58cb65cb71579d5afdfd644e5ef
Şafak Tamses <safak.tamses@gmail.com>
2023-08-03 13:22:11 +0300

    03.08.23
    fixed gitignore

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Switch, Link } from "react-router-dom";
+import Page1 from "./Page/Page1";
+import Page2 from "./Page/Page2";
+import Page3 from "./Page/Page3";
+import Navbar from "./Page/Navbar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Page1 />}></Route>
+        <Route path="/page2" element={<Page2 />}></Route>
+        <Route path="/page3" element={<Page3 />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
